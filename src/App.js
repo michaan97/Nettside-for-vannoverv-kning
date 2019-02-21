ed1777cb15a24c404548d64e50784762736ab11c
elsysgruppe12 <michaan@stud.ntnu.no>
2019-02-21 23:39:15 +0100

    lagt til div ting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
+// components
+import Header from './components/headerComponent/header';
+import Footer from './components/footerComponent/footer';
+import Homepage from './components/pages/homePage';
+// includes
+import './Assets/css/default.min.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      Hello world!!
+
+        <Header />
+          <Homepage />
+        <Footer />
+
+
       </div>
+
     );
   }
 }
---
+++ src/components/pages/homePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+      Home Page Content here
+      
+      </div>
+    );
+  }
+}
+
+export default Homepage;

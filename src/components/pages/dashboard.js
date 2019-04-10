d3c320c0b09334616ccaf370cd834a5de55584e4
Endre Dåvøy <lolskap@live.no>
2019-04-10 11:41:40 +0200

    fix

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {lastHeard, status, toTime} from '../tools/misc';
-import Tempchart from '../subpages/temperature/tempchart.js'
 import Chart from '../charts/Chart.js'
 
 class Dashboard extends Component {
